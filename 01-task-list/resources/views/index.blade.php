498a030edf2f2db922b35f431e6d11419175e02f
itsFDavid <FDavid04@icloud.com>
2025-01-25 18:05:36 -0600

    Initialize Laravel Task List application with basic structure and configuration files

---
+++ 01-task-list/resources/views/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('title', 'List of tasks')
+
+@section('content')
+<div>
+    {{-- @if(count($tasks))
+        @foreach ($tasks as $task )
+            <div>{{$task->title}}</div>
+        @endforeach
+    @else
+        <div>There are no tasks</div>
+    @endif --}}
+
+    @forelse ($tasks as $task)
+        <div>
+            <a href="{{ route('tasks.show', ['id' => $task->id ])}}">{{$task->title}}</a>
+        </div>
+    @empty
+        <div>There are no tasks</div>
+    @endforelse
+</div>
+
+@endsection
---
+++ 01-task-list/routes/web.php
@@ -0,0 +1,109 @@
+<?php
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Route;
+
+// class Task
+// {
+//     public function __construct(
+//         public int $id,
+//         public string $title,
+//         public string $description,
+//         public ?string $long_description,
+//         public bool $completed,
+//         public string $created_at,
+//         public string $updated_at
+//     ) {
+//     }
+// }
+// $tasks = [
+//     new Task(
+//         1,
+//         'Buy groceries',
+//         'Task 1 description',
+//         'Task 1 long description',
+//         false,
+//         '2023-03-01 12:00:00',
+//         '2023-03-01 12:00:00'
+//     ),
+//     new Task(
+//         2,
+//         'Sell old stuff',
+//         'Task 2 description',
+//         null,
+//         false,
+//         '2023-03-02 12:00:00',
+//         '2023-03-02 12:00:00'
+//     ),
+//     new Task(
+//         3,
+//         'Learn programming',
+//         'Task 3 description',
+//         'Task 3 long description',
+//         true,
+//         '2023-03-03 12:00:00',
+//         '2023-03-03 12:00:00'
+//     ),
+//     new Task(
+//         4,
+//         'Take dogs for a walk',
+//         'Task 4 description',
+//         null,
+//         false,
+//         '2023-03-04 12:00:00',
+//         '2023-03-04 12:00:00'
+//     ),
+// ];
+
+Route::get('', function(){
+    return redirect()->route('tasks.index');
+});
+
+Route::get('/tasks', function () {
+    return view('index',[
+        'tasks' => Task::all(),
+    ]);
+})->name('tasks.index');
+
+Route::view('/tasks/create', 'create')->name('tasks.create');
+
+Route::get('tasks/{id}', function($id) {
+
+    return view('show', [
+        'task' => Task::findOrFail($id)
+    ]);
+})->name('tasks.show');
+
+Route::post('/tasks', function(Request $request) {
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'description' => 'required',
+        'long_description' => 'required'
+    ]);
+    $task = new Task;
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
+
+    $task->save();
+
+    return redirect()->route('tasks.show', ['id' => $task->id]);
+})->name('tasks.store');
+
+// Route::get('/noName', function(){
+//     return 'hello';
+// })->name('hello');
+
+// Route::get('/hello/{name}', function($name){
+//     return 'hello ' . $name . ' !';
+// });
+
+// Route::get('/hallo', function(){
+//     return redirect()->route('hello');
+// });
+
+// Route::fallback(function(){
+//     return 'No hay nada aqui';
+// });
