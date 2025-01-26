c274af26c60270862c4aa62c9ffe0bb9aa8ab057
itsFDavid <FDavid04@icloud.com>
2025-01-26 17:10:25 -0600

    feat(task): add toggle complete functionality and update task view

--- 01-task-list/resources/views/show.blade.php
+++ 01-task-list/resources/views/show.blade.php
@@ -12,10 +12,23 @@
 <p>{{$task->created_at}}</p>
 <p>{{$task->updated_at}}</p>
 
+<p>{{$task->completed ? 'Completed' : 'Incomplete'}}</p>
+
 <div>
     <a href="{{route('tasks.edit', ['task' => $task])}}">Edit</a>
 </div>
 {{-- Se puede pasar solo la task ya que laravel usa la clave primaria por defecto, si no le podemos indicar de igual manera --}}
+
+<div>
+    <form action="{{route('tasks.toggle-complete', ['task' =>$task])}}" method="POST">
+        @csrf
+        @method('PUT')
+        <button type="submit">
+            Masrk as {{$task->completed ? 'Incomplete' : 'Complete'}}
+        </button>
+    </form>
+</div>
+
 <div>
     <form
         action="{{route('tasks.destroy', ['task' => $task->id])}}"
