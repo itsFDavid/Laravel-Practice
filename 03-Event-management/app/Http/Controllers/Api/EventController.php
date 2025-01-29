91ef478a58bd7a0b151db3986590b72f835fca1b
itsFDavid <FDavid04@icloud.com>
2025-01-28 19:50:36 -0600

    feat(event-management): implement logout functionality in AuthController and secure EventController routes with authentication middleware

--- 03-Event-management/app/Http/Controllers/Api/EventController.php
+++ 03-Event-management/app/Http/Controllers/Api/EventController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use Illuminate\Routing\Controller;
 use App\Http\Resources\EventResource;
 use App\Http\Traits\CanLoadRelationships;
 use App\Models\Event;
@@ -16,6 +16,10 @@ class EventController extends Controller
     // en este caso se incluiran las relaciones user, attendees y attendees.user
     private array $relations = ['user', 'attendees', 'attendees.user'];
 
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum')->except(['index', 'show']);
+    }
     /**
      * Display a listing of the resource.
      */
@@ -42,7 +46,7 @@ class EventController extends Controller
                 'start_time' => 'required|date',
                 'end_time' => 'required|date|after:start_time'
             ]),
-            'user_id' => 1
+            'user_id' => $request->user()->id,
         ]);
         return new EventResource($this->loadRelationships($event, $this->relations));
     }
