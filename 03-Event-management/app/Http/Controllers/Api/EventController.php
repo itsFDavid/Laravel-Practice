02b620f84b678174579cd971724f316acc7434bb
itsFDavid <FDavid04@icloud.com>
2025-01-28 18:03:20 -0600

    feat(event-management): implement AttendeeController with CRUD operations and add AttendeeResource for API responses

--- 03-Event-management/app/Http/Controllers/Api/EventController.php
+++ 03-Event-management/app/Http/Controllers/Api/EventController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\EventResource;
 use App\Models\Event;
 use Illuminate\Http\Request;
 
@@ -13,7 +14,9 @@ class EventController extends Controller
      */
     public function index()
     {
-        return Event::all();
+        return EventResource::collection(
+            Event::with('user')->paginate()
+        );
     }
 
     /**
@@ -31,7 +34,7 @@ class EventController extends Controller
             ]),
             'user_id' => 1
         ]);
-        return $event;
+        return new EventResource($event);
     }
 
     /**
@@ -39,7 +42,8 @@ class EventController extends Controller
      */
     public function show(Event $event)
     {
-        return $event;
+        $event->load('user', 'attendee');
+        return new EventResource($event);
     }
 
     /**
@@ -55,7 +59,7 @@ class EventController extends Controller
                 'end_time' => 'sometimes|date|after:start_time'
             ])
         );
-        return $event;
+        return new EventResource($event);
     }
 
     /**
