f050b85520df47fe666ba6b87b01c5afa8356da9
itsFDavid <FDavid04@icloud.com>
2025-01-29 18:42:48 -0600

    feat(event-management): implement rate limiting for API requests

--- 03-Event-management/app/Providers/AppServiceProvider.php
+++ 03-Event-management/app/Providers/AppServiceProvider.php
@@ -5,7 +5,10 @@ namespace App\Providers;
 use App\Models\Attendee;
 use App\Models\Event;
 use App\Policies\EventPolicy;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -27,6 +30,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+
+        RateLimiter::for('api', function (Request $request){
+            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+        });
+
         // Gate::define('update-event', function($user, Event $event){
         //     return $user->id === $event->user_id;
         // });
