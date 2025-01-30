5a67638610512ff31c63f3bb54b2e97988c2eb50
itsFDavid <FDavid04@icloud.com>
2025-01-29 18:43:14 -0600

    feat(event-management): implement rate limiting for store and destroy actions in Attendee and Event controllers

--- 03-Event-management/app/Http/Controllers/Api/AttendeeController.php
+++ 03-Event-management/app/Http/Controllers/Api/AttendeeController.php
@@ -21,6 +21,8 @@ class AttendeeController extends Controller
 
     public function __construct(){
         $this->middleware('auth:sanctum')->except(['index', 'show']);
+        $this->middleware('throttle:api')
+            ->only(['store', 'destroy']);
         $this->authorizeResource(Attendee::class, 'attendee');
     }
     /**
@@ -44,7 +46,7 @@ class AttendeeController extends Controller
     {
         $attendee = $this->loadRelationships(
             $event->attendees()->create([
-                    'user_id' => 1
+                    'user_id' => $request->user()->id,
             ]),
             $this->relations
         );
