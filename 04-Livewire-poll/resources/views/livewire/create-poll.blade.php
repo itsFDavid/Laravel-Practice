4648931d51a76a392844face8fb926baa70df983
itsFDavid <FDavid04@icloud.com>
2025-01-30 14:13:05 -0600

    feat(livewire-poll): add functionality to dynamically add poll options in CreatePoll component

--- 04-Livewire-poll/resources/views/livewire/create-poll.blade.php
+++ 04-Livewire-poll/resources/views/livewire/create-poll.blade.php
@@ -1,9 +1,21 @@
 <div>
-    <form>
+    <form >
         <label>Poll Title</label>
         {{-- Se necesita añadir .live para que muestre lo que se escriba al momento --}}
         <input type="text" wire:model.live.debounce.250ms="title">
 
         <h2>Current Title: <span class="text-blue-500">{{ $title }}</span></h2>
+
+        <div class="mb-4 mt-4">
+            <button class="btn" wire:click.prevent="addOption">Add Option</button>
+        </div>
+
+        <div class="mt-4">
+            @foreach($options as $index => $option)
+                <div class="mb-4">
+                    {{ $index}} - {{ $option }}
+                </div>
+            @endforeach
+        </div>
     </form>
 </div>
