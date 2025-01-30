6c9be82ce32ce18b455c4a89d221abf8a2052968
itsFDavid <FDavid04@icloud.com>
2025-01-30 13:27:48 -0600

    feat(livewire-poll): add comment to clarify usage of wire:model.live for real-time input display

--- 04-Livewire-poll/resources/views/livewire/create-poll.blade.php
+++ 04-Livewire-poll/resources/views/livewire/create-poll.blade.php
@@ -1,6 +1,7 @@
 <div>
     <form>
         <label>Poll Title</label>
+        {{-- Se necesita añadir .live para que muestre lo que se escriba al momento --}}
         <input type="text" wire:model.live.debounce.250ms="title">
 
         <h2>Current Title: <span class="text-blue-500">{{ $title }}</span></h2>
