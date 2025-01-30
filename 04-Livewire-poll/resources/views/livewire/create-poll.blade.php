7f0645870798d43eab13f1ac72e980ac75f83bd1
itsFDavid <FDavid04@icloud.com>
2025-01-30 14:34:23 -0600

    feat(livewire-poll): implement poll creation and option removal functionality in CreatePoll component

--- 04-Livewire-poll/resources/views/livewire/create-poll.blade.php
+++ 04-Livewire-poll/resources/views/livewire/create-poll.blade.php
@@ -1,10 +1,8 @@
 <div>
-    <form >
+    <form wire:submit.prevent="createPoll">
         <label>Poll Title</label>
         {{-- Se necesita añadir .live para que muestre lo que se escriba al momento --}}
         <input type="text" wire:model.live.debounce.250ms="title">
-
-        <h2>Current Title: <span class="text-blue-500">{{ $title }}</span></h2>
 
         <div class="mb-4 mt-4">
             <button class="btn" wire:click.prevent="addOption">Add Option</button>
@@ -12,10 +10,17 @@
 
         <div class="mt-4">
             @foreach($options as $index => $option)
-                <div class="mb-4">
-                    {{ $index}} - {{ $option }}
+                <div class="mb-4 bg-gray-100 p-4 rounded-md">
+                    <label class="text-sm">Option {{$index + 1}}</label>
+
+                    <div class="flex gap-2 items-center">
+                        <input type="text" class="bg-white" wire:model="options.{{ $index }}">
+                        <button class="btn bg-white" wire:click.prevent="removeOption({{$index}})">Remove</button>
+                    </div>
                 </div>
             @endforeach
         </div>
+
+        <button type="submit" class="btn">Create Poll</button>
     </form>
 </div>
