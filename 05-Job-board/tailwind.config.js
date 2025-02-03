4a8438c7fec608b57d6089f04792ae40735be3a4
itsFDavid <FDavid04@icloud.com>
2025-02-03 13:25:43 -0600

    feat(text-input): create TextInput component and integrate it into job index view

--- 05-Job-board/tailwind.config.js
+++ 05-Job-board/tailwind.config.js
@@ -1,18 +1,18 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
+import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './resources/**/*.blade.php',
-        './resources/**/*.js',
-        './resources/**/*.vue',
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
     ],
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
             },
         },
     },
-    plugins: [],
+    plugins: [require("@tailwindcss/forms")],
 };
