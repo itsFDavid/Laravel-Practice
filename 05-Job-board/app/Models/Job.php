951558bfd3c403307a64e0402bca73bebcf39e84
itsFDavid <FDavid04@icloud.com>
2025-02-04 14:08:37 -0600

    feat(employer): add Employer model, migration, and factory with relationships to Job and User models

--- 05-Job-board/app/Models/Job.php
+++ 05-Job-board/app/Models/Job.php
@@ -5,12 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Query\Builder as QueryBuilder;
 
 class Job extends Model
 {
     /** @use HasFactory<\Database\Factories\JobFactory> */
     use HasFactory;
+
+    public function employer(): BelongsTo{
+        return $this->belongsTo(Employer::class);
+    }
 
     public static array $experience = ['entry', 'intermediate', 'senior'];
     public static array $category = ['IT', 'Sales', 'Finance', 'Marketing', 'Engineering'];
