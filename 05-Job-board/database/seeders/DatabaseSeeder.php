750a995bba9d5b863ef341b12873f9346eacd9d7
itsFDavid <FDavid04@icloud.com>
2025-02-04 18:33:18 -0600

    feat(auth): implement authentication controller with login and logout functionality

--- 05-Job-board/database/seeders/DatabaseSeeder.php
+++ 05-Job-board/database/seeders/DatabaseSeeder.php
@@ -15,6 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        User::factory()->create([
+            'name' => 'Test',
+            'email'=> 'test@test.com',
+        ]);
         User::factory(300)->create();
         $users = User::all()->shuffle();
 
