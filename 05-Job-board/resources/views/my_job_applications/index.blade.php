0ca8da70c500e1e9ed89b429b7bcbc184280b066
itsFDavid <FDavid04@icloud.com>
2025-02-06 15:30:55 -0600

    feat(job-application): implement job application deletion and update view with cancel button

--- 05-Job-board/resources/views/my_job_applications/index.blade.php
+++ 05-Job-board/resources/views/my_job_applications/index.blade.php
@@ -6,7 +6,7 @@
     />
 
     @forelse ($applications as $application)
-        <x-job-card :job="$application->job ">
+        <x-job-card :job="$application->job">
             <div class="flex items-center justify-between text-xs text-slate-500">
                 <div>
                     <div>Applied {{ $application->created_at->diffForHumans() }}</div>
@@ -15,18 +15,26 @@
                         {{ $application->job->job_applications_count - 1}}
                     </div>
                     <div>
-                        Your asking salary ${{ number_format($application->expected_alary) }}
+                        Your asking salary ${{ number_format($application->expected_salary) }}
                     </div>
                     <div>
                         Average asking salary ${{ number_format($application->job->job_applications_avg_expected_salary) }}
                     </div>
                 </div>
-                <div></div>
+                <div>
+                    <form action="{{ route('my-job-applications.destroy', $application)}}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <x-button type="submit">Cancel</x-button>
+                    </form>
+                </div>
             </div>
         </x-job-card>
-
     @empty
-
+        <div class="text-center text-slate-500">
+            <p>You have not applied to any jobs yet.</p>
+            <p>Find a job you like and apply now!</p>
+        </div>
     @endforelse
 
 </x-layout>
